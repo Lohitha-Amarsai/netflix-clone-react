b0b035e6a48ce804069f9c5b7e74ca499d478ae1
Hari Charan Kandregula <haricharanwork@gmail.com>
2023-10-15 16:11:11 +0530

    feature(proj:Netflix): Added banner and navbar api, deployed on firebase.

--- netflix_clone/src/App.js
+++ netflix_clone/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Row from './Row';
 import requests from './request';
+import Banner from './Banner';
+import Nav from './Nav';
 
 function App() {
   return (
-    <div className="App">
-      <Row title ='Netflix Originals' fetchUrl = {requests.fetchNetflixOriginals}/>
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row title ='NETFLIX ORIGINALS' fetchUrl = {requests.fetchNetflixOriginals} isLargeRow/>
       <Row title ='Trending now' fetchUrl = {requests.fetchTrending}/>
       <Row title ='Top rated' fetchUrl = {requests.fetchToprated}/>
       <Row title ='Action movies' fetchUrl = {requests.fetchActionMovies}/>
--- netflix_clone/src/Row.js
+++ netflix_clone/src/Row.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import instance from './axios';
 import './Row.css'
+import Youtube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const baseImageUrl = 'https://image.tmdb.org/t/p/original/'
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState('');
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        },
+      };
 
     useEffect(() => {
         async function fetchData () {
@@ -17,8 +28,24 @@ function Row({ title, fetchUrl }) {
         fetchData();
     },[fetchUrl]);
 
+    const handleClick = movie => {
+        if(trailerUrl !== ''){
+            setTrailerUrl('');
+        } else {
+            movieTrailer(movie?.name || '')
+            .then(url => {
+                // https://www.youtube.com/watch?v=XtMThy8QKqU&t
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get('v')); //v=XtMThy8QKqU&t
 
-    console.log(movies);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
+
+    }
+
   return (
     <div className='row'>
         <h2>{title}</h2>
@@ -26,11 +53,14 @@ function Row({ title, fetchUrl }) {
             {/* container --> posters */}
             {movies.map(movie => (
                 <img 
-                className='row_poster'
-                src={`${baseImageUrl}${movie.poster_path}`}
+                key = {movie.id}
+                onClick = {()=> handleClick(movie)}
+                className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
+                src={`${baseImageUrl}${isLargeRow? movie.poster_path : movie.backdrop_path}`}
                 alt={movie.name}/>
             ))}
         </div>
+        {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
       
     </div>
   )
